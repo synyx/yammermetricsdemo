b1eba8813c01591e122cb6f5c7571012f8428909
Joachim Arrasz <arrasz@synyx.de>
2013-04-27 09:11:43 +0200

    * added a demo arithmetic operation to simulate some calculation time

---
+++ src/main/java/com/steinberg/elicenser/demos/ArithmeticDemoOperation.java
@@ -0,0 +1,17 @@
+/*
+ * 22.04.2013
+ */
+package com.steinberg.elicenser.demos;
+
+/**
+ * Simulates a pretty long arithmetic calculation.
+ * 
+ * @author Joachim Arrasz arrasz@synyx.de
+ */
+public class ArithmeticDemoOperation {
+    
+    public void calculateSomeMagic() throws InterruptedException {
+        Thread.sleep(1250);
+    }
+    
+}
